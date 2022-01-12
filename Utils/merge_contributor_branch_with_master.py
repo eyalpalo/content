31db1015de912e36bfbe6458d76afb10628fae30
eyalpalo <epintzov@paloaltonetworks.com>
2022-01-12 16:39:07 +0200

    new script

---
+++ Utils/merge_contributor_branch_with_master.py
@@ -0,0 +1,22 @@
+import logging
+import subprocess
+import sys
+
+CONTENT_URL = 'https://github.com/demisto/content.git'
+
+
+def main():
+    try:
+        subprocess.run(['git', 'remote', 'add', 'upstream', CONTENT_URL])
+        subprocess.run(['git', 'fetch', 'upstream'])
+        subprocess.run(['git', 'checkout', 'master'])
+        subprocess.run(['git', 'rebase', 'upstream/master'])
+        subprocess.run(['git', 'push', '-f', 'origin', 'master'])
+    except Exception as e:
+        logging.exception(f'An error occured while trying to fetch and rebase from content master. {e}')
+        sys.exit(1)
+    logging.debug("fetch and rebase from content master is done.")
+
+
+if __name__ == "__main__":
+    main()
